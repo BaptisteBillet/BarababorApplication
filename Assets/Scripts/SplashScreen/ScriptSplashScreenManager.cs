e4cc46dcebc8824e15c69f4a152a9cccb4d5f472
daviddelseray <ddelseray@gmail.com>
2015-09-02 10:36:26 +0200

    Création du SplashScreen

---
+++ Assets/Scripts/SplashScreen/ScriptSplashScreenManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptSplashScreenManager : MonoBehaviour {
+
+	public void LoadNextScene()
+    {
+        Application.LoadLevel("GeneralInterfaceScene");
+    }
+}
