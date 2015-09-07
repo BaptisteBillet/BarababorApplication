3c7585b2f728377ae0957b442d9e0a35ebe18dd6
daviddelseray <ddelseray@gmail.com>
2015-09-07 16:03:39 +0200

    Systeme description items shop

--- Assets/Scripts/MainScene/ScriptsPanel/ScriptShopDescriptionPanel.cs
+++ Assets/Scripts/MainScene/ScriptsPanel/ScriptShopDescriptionPanel.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScriptShopDescriptionPanel : MonoBehaviour
 {
 
     ScriptBuyableItems m_ScriptBuyableItems;
+
+    public Text m_DescriptionField;
+
+    public Button m_BackButton;
+
+    public Image m_ItemIcon;
+
 
     public void Activation(GameObject SelectedItem)
     {
@@ -12,6 +20,15 @@ public class ScriptShopDescriptionPanel : MonoBehaviour
 
         m_ScriptBuyableItems = SelectedItem.GetComponent<ScriptBuyableItems>();
 
+        m_DescriptionField.text = m_ScriptBuyableItems.m_Description;
+
+       
+
     }
 	
+    public void Desable ()
+    {
+        this.gameObject.SetActive(false);
+
+    }
 }
