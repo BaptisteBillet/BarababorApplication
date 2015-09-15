ddf849b47f8e5dd17b227399f819b98b5c579255
daviddelseray <ddelseray@gmail.com>
2015-09-15 17:30:02 +0200

    Poursuite Shop Captain

--- Assets/Scripts/MainScene/ScriptsPanel/Shop/Captain/ScriptGeneralCaptainShop.cs
+++ Assets/Scripts/MainScene/ScriptsPanel/Shop/Captain/ScriptGeneralCaptainShop.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class ScriptGeneralCaptainShop : MonoBehaviour {
 
+    public GameObject m_Captain;
+    public GameObject m_FirstDisplayStand;
+
+    public GameObject m_CurrentDisplayStand;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        m_Captain.SetActive(true);
+        m_Captain.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+        m_Captain.transform.position = new Vector3(m_Captain.transform.position.x, 0.62f, m_Captain.transform.position.z);
+
+        LoadDisplayStand(m_FirstDisplayStand);
 	}
+
+    public void LoadDisplayStand(GameObject DisplayStand)
+    {
+        DisplayStand.SetActive(true);
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    public void UnloadDisplayStand ()
+    {
+        m_CurrentDisplayStand.SetActive(false);
+    }
 	
-	}
 }
