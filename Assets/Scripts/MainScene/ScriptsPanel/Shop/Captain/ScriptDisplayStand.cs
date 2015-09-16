6931dcaf4e3583afd607038dbeba5789a6a56c86
daviddelseray <ddelseray@gmail.com>
2015-09-16 17:32:43 +0200

    Itération shop

--- Assets/Scripts/MainScene/ScriptsPanel/Shop/Captain/ScriptDisplayStand.cs
+++ Assets/Scripts/MainScene/ScriptsPanel/Shop/Captain/ScriptDisplayStand.cs
@@ -14,7 +14,7 @@ public class ScriptDisplayStand : MonoBehaviour {
                 break;
         }
 
-       // foreach ()
+       
 	
 	}
 	
--- Assets/Scripts/MainScene/ScriptsPanel/Shop/Ship/ScriptShopPanel.cs
+++ Assets/Scripts/MainScene/ScriptsPanel/Shop/Ship/ScriptShopPanel.cs
@@ -1,16 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ScriptShopPanel : MonoBehaviour {
 
     public Text m_CoinsMetersField;
     public Text m_DiamondsMetersField;
 
-    
+    public List<string> m_NoseCatalog = new List<string>();
+    public List<string> m_EarsCatalog = new List<string>();
+    public List<string> m_EyesCatalog = new List<string>();
+    public List<string> m_MouthCatalog = new List<string>();
 
-	// Use this for initialization
-	void Start ()
+    public List<string> m_JacketCatalog = new List<string>();
+    public List<string> m_BootsCatalog = new List<string>();
+    public List<string> m_PantsCatalog = new List<string>();
+    public List<string> m_HatCatalog = new List<string>();
+
+
+    // Use this for initialization
+    void Start ()
     {
         CurrencyMetersUpdate();
 	}
