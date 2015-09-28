09dc3210df8aa335f48669ce376d829aa6fa8e59
daviddelseray <ddelseray@gmail.com>
2015-09-28 16:47:37 +0200

    itération drugBasket

--- Assets/Scripts/DrugBasket/ScriptDrugsBasket.cs
+++ Assets/Scripts/DrugBasket/ScriptDrugsBasket.cs
@@ -3,9 +3,19 @@ using System.Collections;
 
 public class ScriptDrugsBasket : MonoBehaviour
 {
+
+    void Start ()
+    {
+        Location();
+    }
+
     void OnTriggerEnter (Collider other)
     {
-        Debug.Log("OY");
+        //Debug.Log("OY");
     }
 	
+    public void Location()
+    {
+        this.transform.position =new Vector3 (Random.Range(-2f, 2f),-1.5f,20.36f);
+    }
 }
--- Assets/Scripts/DrugBasket/ScriptHand.cs
+++ Assets/Scripts/DrugBasket/ScriptHand.cs
@@ -13,10 +13,11 @@ public class ScriptHand : MonoBehaviour
 
     public float m_Shoot;
 
-    bool m_Moving;
+    public bool m_Moving;
 
-    bool m_IsShot;
+    public bool m_IsShot;
 
+    bool m_IsReady;
 	
 
     void Start ()
@@ -40,19 +41,45 @@ public class ScriptHand : MonoBehaviour
 
         }
        
-       
+       if (m_Pill.transform.position.y<-1f)
+        {
+            ComeBack();
+        }
     }
 
     public void Shoot()
     {
-        if (m_IsShot==false)
-        {
-            m_Moving = false;
-            m_IsShot = true;
-            m_PillRB.AddForce(new Vector3(0f, m_Shoot, m_Shoot));
-            m_PillRB.useGravity = true;
-        }
-       
+      
+           
+            Debug.Log(m_IsShot);
+            if (m_IsShot == false)
+            {
 
+                m_Moving = false;
+                m_IsShot = true;
+
+                m_PillRB.AddForce(new Vector3(0f, m_Shoot, m_Shoot));
+                Debug.Log(m_Shoot);
+
+                m_PillRB.useGravity = true;
+            }
+
+        
+
+    }
+
+   /* public void OnMouseUp()
+    {
+        m_IsReady = true;
+    }
+    */
+    public void ComeBack()
+    {
+        m_PillRB.useGravity = false;
+        m_Pill.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.61f, this.transform.position.z + 0.72f);
+        m_IsShot = false;
+        m_Moving = true;
+        
+        
     }
 }
